28296ba5d0762f280d27ed96e00f996cd40d9905
Ali Khakpouri <akhakpouri@r2integrated.com>
2017-05-15 16:17:41 -0400

    Finished the day 2 challenge.

---
+++ c#/ThirtyDaysOfCode/MealCost/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ThirtyDaysOfCode.MealCost
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            double mealCost;
+            int tipPercent;
+            int taxPercent;
+            double.TryParse(Console.ReadLine(), out mealCost);
+            int.TryParse(Console.ReadLine(), out tipPercent);
+            int.TryParse(Console.ReadLine(), out taxPercent);
+
+            double tip = (mealCost *  tipPercent) / 100;
+            double tax = (mealCost * taxPercent) / 100;
+            var totalCost = Math.Round(mealCost + tip + tax, 0);
+            Console.WriteLine($"The total meal cost is {totalCost} dollars.");
+            Console.ReadLine();
+
+
+        }
+    }
+}
