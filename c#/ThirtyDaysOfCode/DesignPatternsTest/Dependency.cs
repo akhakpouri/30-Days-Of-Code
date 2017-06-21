0dda48ddb0fbbd62a12b4cae30a22bdd601922e3
Ali Khakpouri <akhakpouri@r2integrated.com>
2017-06-21 11:02:14 -0400

    Introduced constructor and property dependency injections.

---
+++ c#/ThirtyDaysOfCode/DesignPatternsTest/Dependency.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using DesignPatterns.Models.DI;
+using DesignPatterns.Models.DI.CI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DesignPatternsTest
+{
+    [TestClass]
+    public class Dependency
+    {
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            var client = new Client(new Service());
+            client.Start();
+        }
+
+        [TestMethod]
+        public void PropertyTest()
+        {
+            var client = new DesignPatterns.Models.DI.PI.Client()
+            {
+                Service = new Service()
+            };
+            client.Start();
+        }
+    }
+}
