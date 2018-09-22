18e2c84e337a719bf7835df2ebcce650d6895693
Ali Khakpouri <khakpouria@tessco.com>
2018-09-22 13:04:18 -0400

    Finished the factory pattern. Next, I'll have to add some unit test to it.

---
+++ c#/DesignPatterns/Factory/FactoryPattern.Data/Factories/CableFactory.cs
@@ -0,0 +1,13 @@
+﻿using FactoryPattern.Models;
+using FactoryPattern.Models.Products;
+
+namespace FactoryPattern.Factories
+{
+    public class CableFactory : IProductFactory
+    {
+        public IProduct CreateProduct()
+        {
+            return new Cable();
+        }
+    }
+}
---
+++ c#/DesignPatterns/Factory/FactoryPattern.Data/Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FactoryPattern.Models
+{
+    public abstract class Product : IProduct
+    {
+        public string Name { get; set; }
+        public string Sku { get; set; }
+        public ProductType ProductType { get; set; }
+        public void Allign()
+        {
+            Console.WriteLine($"{nameof(IProduct)} is about to get alligned.");
+        }
+        public void Cut()
+        {
+            Console.WriteLine($"{nameof(IProduct)} is about to get cut.");
+        }
+
+        protected Product()
+        {
+
+        }
+    }
+}
