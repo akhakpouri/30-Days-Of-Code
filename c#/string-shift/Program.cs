d36bd18e4c7a35030eebc10cce0beb199400af25
Ali Khakpouri <ali.khakpouri@gmail.com>
2020-12-22 10:49:21 -0500

    Fixed the agenda

--- c#/string-shift/Program.cs
+++ c#/string-shift/Program.cs
@@ -4,11 +4,10 @@ namespace StringShift
 {
     class Program
     {
-        ///Check if a string can be obtained byu rotating another string 2 places. 
+        ///Check if two strings are a rtoation of one and other or not.
         static void Main(string[] args)
         {
-            //Given the two strings below, the task is to find if a string can be obtained by rotating another string 2 places.
-            //string 1: amazon. string 2: azonam -- true. rotating anti-clockwise
+            //string 1: amazon. string 2: azonam -- true. it is rotating anti-clockwise
             //string 1: amazon. string 2: onamaz -- true. rotating clockwise
             //stirng 1: amazon. string 2; zonmaz -- false.
             string first = "amazon";
